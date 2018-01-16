c6181fb26962a54b3cab40f0f5f5753268071413
Maxim <b3tmanann@gmail.com>
2018-01-16 12:13:16 +0300

    Добавил segmented control, переключаются экраны

---
+++ TestApp/Modules/RegisterViewController/RegisterViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RegisterViewController.swift
+//  TestApp
+//
+//  Created by Максим Бриштен on 16.01.2018.
+//  Copyright © 2018 Максим Бриштен. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
